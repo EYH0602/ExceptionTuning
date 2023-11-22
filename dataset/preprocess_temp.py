c89d2388db0dcf39fa2fdd781630cbbed513d346
Yifeng He <yfhe@ucdavis.edu>
2023-11-21 18:33:37 -0800

    format

--- dataset/preprocess_temp.py
+++ dataset/preprocess_temp.py
@@ -10,25 +10,19 @@ import signal
 from transformers import RobertaTokenizer
 import matplotlib.pyplot as plt
 
-f_train = open("train_err_cls.jsonl", 'w')
-f_valid = open("valid_err_cls.jsonl", 'w')
-f_test = open("test_err_cls.jsonl", 'w')
-cont  = 0
-
-directory_path = './Project_CodeNet_Python800/'
-fuzz_dir_path = './fuzz/'
 
 def read_err_file(path):
-    input = open(path, 'r', encoding='utf-8').read()
+    input = open(path, "r", encoding="utf-8").read()
     return input
 
-def find_errorType(text):
+
+def find_error_type(text):
     lines = text.splitlines()
 
-# Extract the string before ":" in the fourth line
+    # Extract the string before ":" in the fourth line
     if lines:
         last_line = lines[-1].strip()
-        index_colon = last_line.find(':')
+        index_colon = last_line.find(":")
 
         if index_colon != -1:
             extracted_string = last_line[:index_colon].strip()
@@ -36,7 +30,8 @@ def find_errorType(text):
     else:
         return "No Error"
 
-def find_errorLine(text):
+
+def find_error_line(text):
     lines = text.splitlines()
     if len(lines) > 1:
         second_line_from_bottom = lines[-2].strip()
@@ -44,57 +39,68 @@ def find_errorLine(text):
     else:
         return " "
 
+
 # initialize for two plots
 err_dict = {}
 
 err_type_dict = {}
 
+f_train = open("train_err_cls.jsonl", "w")
+f_valid = open("valid_err_cls.jsonl", "w")
+f_test = open("test_err_cls.jsonl", "w")
+cont = 0
+
+directory_path = "./Project_CodeNet_Python800/"
+fuzz_dir_path = "./fuzz/"
+
 
 for dir in sorted(os.listdir(directory_path)):
-    if dir != '.DS_Store':
+    if dir != ".DS_Store":
         full_path = os.path.join(directory_path, dir)
         for f in os.listdir(full_path):
             item = os.path.join(f)
             # print(item)
             r = random.random()
             if r < 0.6:
-                split = 'train'
+                split = "train"
             elif r < 0.8:
-                split = 'valid'
+                split = "valid"
             else:
-                split = 'test'
+                split = "test"
             # js['index']=str(cont)
-            err_dict[dir+'/'+item] = []
+            err_dict[dir + "/" + item] = []
 
-            fuzz_path = os.path.join(fuzz_dir_path, dir, item) + '/' + 'default/'
+            fuzz_path = os.path.join(fuzz_dir_path, dir, item) + "/" + "default/"
             if os.path.exists(fuzz_path):
-                err_path = fuzz_path + 'stderr/'
+                err_path = fuzz_path + "stderr/"
                 for file in sorted(os.listdir(err_path)):
                     err_file = os.path.join(err_path, file)
 
                     output = read_err_file(err_file)
-                    error_str = find_errorType(output)
-                    err_dict[dir+'/'+item].append(error_str)
-                    
+                    error_str = find_error_type(output)
+                    err_dict[dir + "/" + item].append(error_str)
+
                     if error_str not in err_type_dict:
                         err_type_dict[error_str] = 1
                     else:
                         err_type_dict[error_str] += 1
 
-                    js={}
+                    js = {}
 
-                    js['label']= error_str
-                    js['error']= find_errorLine(output)
-                    js['path'] = err_file
-                    js['code']=open(os.path.join(full_path,item),encoding='latin-1').read()
+                    js["label"] = error_str
+                    js["error"] = find_error_line(output)
+                    js["path"] = err_file
+                    js["code"] = open(
+                        os.path.join(full_path, item), encoding="latin-1"
+                    ).read()
 
                     # print(js['label'], js['error'], err_file)
-                    if split == 'train':
-                        f_train.write(json.dumps(js)+'\n')
-                    elif split == 'valid':
-                        f_valid.write(json.dumps(js)+'\n')
-                    elif split == 'test':
-                        f_test.write(json.dumps(js)+'\n')
+                    if split == "train":
+                        f_train.write(json.dumps(js) + "\n")
+                    elif split == "valid":
+                        f_valid.write(json.dumps(js) + "\n")
+                    elif split == "test":
+                        f_test.write(json.dumps(js) + "\n")
                     else:
                         raise NotImplementedError
             # cont+=1
@@ -104,34 +110,30 @@ f_valid.close()
 f_test.close()
 
 # this is the dict for the first plot
-uniq_dict= {}
+uniq_dict = {}
 for i in range(len(err_type_dict)):
     uniq_dict[i] = 0
 
 for key in err_dict:
     err_dict[key] = list(set(err_dict[key]))
 
-    if 'No Error' not in err_dict[key]:
-        uniq_dict[len(err_dict[key])] +=1
+    if "No Error" not in err_dict[key]:
+        uniq_dict[len(err_dict[key])] += 1
     else:
         if len(err_dict[key]) == 1:
             uniq_dict[0] += 1
-        else: 
-            uniq_dict[len(err_dict[key])-1] +=1
+        else:
+            uniq_dict[len(err_dict[key]) - 1] += 1
 print(uniq_dict)
 print(err_type_dict)
-plt.bar(uniq_dict.keys(), uniq_dict.values(),  align='center', alpha=0.7)
-plt.xlabel('number of unique error(s)')
-plt.ylabel('number of solution(s)')
-plt.title('Histogram of the number of unique error(s)')
-plt.savefig('unique_errors.png')
+plt.bar(uniq_dict.keys(), uniq_dict.values(), align="center", alpha=0.7)
+plt.xlabel("number of unique error(s)")
+plt.ylabel("number of solution(s)")
+plt.title("Histogram of the number of unique error(s)")
+plt.savefig("unique_errors.png")
 
-plt.bar(err_type_dict.keys(), err_type_dict.values(), align='center', alpha=0.7)
-plt.xlabel('Types of Error')
-plt.ylabel('number of errors')
-plt.title('Bar plot of the number of errors in each type')
-plt.savefig('type_cnt_errors.png')
-
-    
-
-
+plt.bar(err_type_dict.keys(), err_type_dict.values(), align="center", alpha=0.7)
+plt.xlabel("Types of Error")
+plt.ylabel("number of errors")
+plt.title("Bar plot of the number of errors in each type")
+plt.savefig("type_cnt_errors.png")
