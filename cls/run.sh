36f8b929c25369e0b60ad33eaadeb266aa6f3f53
Ethan He <yfhe@ucdavis.edu>
2023-11-28 00:56:16 +0000

    only train 1 epoch

--- cls/run.sh
+++ cls/run.sh
@@ -5,7 +5,7 @@ ifraw='False'
 error_prompt='error is in line:'
 n_class=22
 
-CUDA_VISIBLE_DEVICES=0,1 python3 run.py \
+CUDA_VISIBLE_DEVICES=3,4 python3 run.py \
                 --output_dir=./savedmodels \
                 --model_type=roberta_cls \
                 --config_name=../microsoft/$model \
@@ -16,7 +16,7 @@ CUDA_VISIBLE_DEVICES=0,1 python3 run.py \
                 --train_data_file=../dataset/train_err_cls.jsonl \
                 --eval_data_file=../dataset/valid_err_cls.jsonl \
                 --test_data_file=../dataset/test_err_cls.jsonl \
-                --epoch 10 \
+                --epoch 1 \
                 --block_size 512 \
                 --train_batch_size 32 \
                 --eval_batch_size 64 \
@@ -28,4 +28,4 @@ CUDA_VISIBLE_DEVICES=0,1 python3 run.py \
                 --nohang \
                 --nocrash \
                 --n_class $n_class \
-                --seed 123456 2>&1| tee ./log.txt &
+                --seed 123456 2>&1| tee ./exception_train.log.txt &
